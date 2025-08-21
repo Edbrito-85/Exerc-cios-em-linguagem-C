4bbc64c307caeaba2d3e44e60855c53c4ec73fc1
Edbrito-85 <edbrito.3570@hotmail.com>
2025-08-21 17:44:04 -0300

    Add files via upload

---
+++ Desafio Novato Game War.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Criando a estruct Território
+struct Territorio{
+	char nome [30];
+	char cor [10];
+	int tropas;	
+	};
+
+// Renomeando a struct para Território	
+typedef struct Territorio Territorio;
+
+int main (){
+// Declarando o vetor de struct
+int Territorio [5];	
+
+
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
